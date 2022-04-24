ded025c328fa06aacf67b1d4129547023ecf8310
Epicrider <ohossain@uci.edu>
2022-04-24 14:38:25 -0700

    Recieve (Incorrect/Random) Data from Transmitter

--- src/main.cpp
+++ src/main.cpp
@@ -1,82 +1,26 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-/*  ~ Simple Arduino - xBee Receiver sketch ~ Coordinator
- */
+char incomingByte = '0';
+// SoftwareSerial XBee(0, 1);
 
-SoftwareSerial zigbee(12, 11);
-char received;
-
-bool started = false; // True: Message is strated
-bool ended = false;   // True: Message is finished
-byte i;           // Index of array
-
-char character; // Variable to store the incoming byte
-char msg[13];   // Message - array
-
-
-
-void packetCheck() {
-
-  while (Serial.available())
-  {
-    character = Serial.read();
-    if (character == '<')
-    {
-      started = true;
-      i = 0;
-      msg[i] = '\0'; // Throw away any incomplete packet
-    }
-
-    // End the message when the '>' symbol is received
-    else if (character == '>')
-    {
-      ended = true;
-      break; // Done reading - exit from while loop!
-    }
-
-    // Read the message!
-    else
-    {
-      if (i < 11)
-      {                         // Make sure there is room
-        msg[i] = character; // Add char to array
-        i++;
-        msg[i] = '\0'; // Add NULL to end
-      }
-    }
-  }
-
-  if (started && ended)
-  {
-    Serial.print("Message: ");
-    Serial.println(msg);
-
-    i = 0;
-    msg[i] = '\0';
-    started = false;
-    ended = false;
-  }
-
+void setup() {
+  // Serial Ports for Receiving
+  Serial1.begin(11520);
+  Serial.begin(11520);
 }
 
-void setup()
-{
-  Serial.begin(9600);
-  zigbee.begin(9600);
-}
-
-void loop()
-{
-
-  if (zigbee.available() > 0) {
-    received = zigbee.read();
-    if(received == '0') {
-      Serial.print("Off");
-    }
-    else if(received == '1') {
-      Serial.println("On");
-    }
+void loop() {
+  // Wait until package arrives
+  while(Serial1.available()) {
+//    incomingByte = Serial1.read();
+//    Serial.print("I recieved: ");
+//    Serial.println(incomingByte);
+      incomingByte = Serial1.read();
+      Serial.print((char)incomingByte);
   }
-  // packetCheck();
+  while(Serial.available() != 0) {
+    Serial.print(Serial.read());
+  }
+  
 }
